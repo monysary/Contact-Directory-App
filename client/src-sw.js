8187af4b3bb9bd1a70be49d2e0f2efd32667db86
monysary <sary.mony@gmail.com>
2023-01-18 13:59:04 -0800

    pushing to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,23 @@
 // TODO: Create a service worker that caches static assets:
+const { StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      // Name of the cache storage.
+      cacheName: 'asset-cache',
+      plugins: [
+        // This plugin will cache responses with these headers to a maximum-age of 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    })
+);
+  
